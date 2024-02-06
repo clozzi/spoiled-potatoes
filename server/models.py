8643357fcd302c0e525293df6e6dfb38dddb8ade
Chris Lozzi <clozzi12@gmail.com>
2024-02-06 16:21:57 -0500

    basic models created

--- server/models.py
+++ server/models.py
@@ -7,3 +7,26 @@ from config import db
 
 # what to do what to do
 # insert models here
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Integer)
+
+    reviews = db.relationship('Review', back_populates='')
+    
+
+class Media(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    reviews = db.relationship('Review', back_populates='')
+    
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer)
+    comment = db.Column(db.Integer)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    media_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    # relationships?
