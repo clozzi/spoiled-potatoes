9180e35438fe25e5041dc9b4f91d622da0e5ba0e
Chris Lozzi <clozzi12@gmail.com>
2024-02-07 16:55:47 -0500

    MediaCard CSS

--- client/src/components/MediaCard.js
+++ client/src/components/MediaCard.js
@@ -4,12 +4,12 @@ function MediaCard({ media }) {
 
 
     return (
-        <li>
+        <div className="mediaCard">
             <img src={media.image_url} alt="media"/>
             <h1>{media.title}</h1>
             <h3>{media.media_type}</h3>
             <h3>Streaming on: {media.streaming_platform}</h3>
-        </li>
+        </div>
     )
 }
 
