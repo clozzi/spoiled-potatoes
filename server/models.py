9b30c3b49d5de47be5df5f582db14151683df43b
Chris Lozzi <clozzi12@gmail.com>
2024-02-07 11:18:55 -0500

    + media validations

--- server/models.py
+++ server/models.py
@@ -2,29 +2,56 @@
 
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
 
 from config import db
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Integer)
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.Integer)
 
-    reviews = db.relationship('Review', back_populates='')
+#     reviews = db.relationship('Review', back_populates='')
     
 
 class Media(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    media_type = db.Column(db.String)
+    streaming_platform = db.Column(db.String)
+    title = db.Column(db.String)
+    image_url = db.Column(db.String)
 
-    reviews = db.relationship('Review', back_populates='')
+    @validates('media_type')
+    def validate_type(self, key, media_type):
+        media_types = ['Movie', 'Series']
+
+        if media_type not in media_types:
+            raise ValueError('Media Type must be Movie or Series')
+        return media_type
+    
+    @validates('streaming_platform')
+    def validate_streaming(self, key, platform):
+        platforms = ['HBO', 'Max', 'Hulu', 'Netflix', 'Prime', 'Peacock']
+
+        if platform not in platforms:
+            raise ValueError('Must be one of the big 6')
+        return platform
+    
+    @validates('title')
+    def validate_title(self, key, title):
+        if not (1 <= len(title) <= 32):
+            raise ValueError('Incorrect title length')
+        return title
+
+#     reviews = db.relationship('Review', back_populates='')
     
 
-class Review(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    rating = db.Column(db.Integer)
-    comment = db.Column(db.Integer)
+# class Review(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     rating = db.Column(db.Integer)
+#     comment = db.Column(db.Integer)
 
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    media_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#     media_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     # relationships?
