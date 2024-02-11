06e33404c63ca55291517226cd61d2e6b544e470
Chris Lozzi <clozzi12@gmail.com>
2024-02-11 16:07:08 -0500

    home/signup/login/search --base only

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -16,23 +16,21 @@ function Home() {
       <main>
         <h1><img src={potato} alt="spoiled potato" width="30" height="30" />Welcome to Spoiled Potatoes!<img src={potato} alt="spoiled potato" width="30" height="30" /></h1>
         {medias.map((media) => (
-          <div>
-            <div className="medias" key={media.id} >
-              <img src={media.image_url} alt="media" width="100" height="100"/>
-              <h3>{media.title}</h3>
-              <h5>{media.media_type}</h5>
-              <h5>Streaming on: {media.streaming_platform}</h5>
-              <div>
-              {media.reviews.map((review) => (
-                  <div key={review.id}>
-                  <p>Rating: {review.rating}</p>
-                  <p>Explanation:{review.comment}</p>
-                  <p>User: {review.user.username}</p>
-                  </div>
-              ))}
-              </div>
-              <Link to={`/medias/${media.id}`}>Click for more information</Link>
+          <div className="medias" key={media.id} >
+            <img src={media.image_url} alt="media" width="100" height="100"/>
+            <h3>{media.title}</h3>
+            <h5>{media.media_type}</h5>
+            <h5>Streaming on: {media.streaming_platform}</h5>
+            <div>
+            {media.reviews.map((review) => (
+                <div key={review.id}>
+                <p>Rating: {review.rating}</p>
+                <p>Explanation:{review.comment}</p>
+                <p>User: {review.user.username}</p>
+                </div>
+            ))}
             </div>
+            <Link to={`/medias/${media.id}`}>Click for more information</Link>
           </div>
         ))}
       </main>
