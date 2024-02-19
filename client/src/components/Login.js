de36832be5e2688f7a683d9fdbafeb283ed791e4
Chris Lozzi <clozzi12@gmail.com>
2024-02-19 11:50:26 -0500

    cleaned up Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,23 +1,7 @@
-// import { useState } from "react";
 import { useFormik } from "formik";
 import * as yup from "yup";
 
 function Login({ onLogin }) {
-    // const [username, setUsername] = useState("")
-    // const [password, setPassword] = useState("")
-
-    // function handleSubmit(e) {
-    //     e.preventDefault()
-    //     fetch("/login", {
-    //         method: "POST",
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //         },
-    //         body: JSON.stringify({ username }),
-    //     })
-    //         .then((r) => r.json())
-    //         .then((user) => onLogin(user))
-    // }
 
     const formSchema = yup.object().shape({
         username: yup.string().required("Must enter username").max(20),
@@ -41,7 +25,6 @@ function Login({ onLogin }) {
                 .then((r) => r.json())
                 .then((data) => {
                     onLogin(data)
-                    console.log(data)
                 })
         }
     })
@@ -73,16 +56,6 @@ function Login({ onLogin }) {
                 <button type="submit">Submit</button>
             </form>
         </div>
-        // <form onSubmit={handleSubmit}>
-        //     <h3>Login with Username</h3>
-        //     <input
-        //         type="text"
-        //         id="username"
-        //         value={username}
-        //         onChange={(e) => setUsername(e.target.value)}
-        //     />
-        //     <button type="submit">Login</button>
-        // </form>
     )
 }
 
